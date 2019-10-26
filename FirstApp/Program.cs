2829b592e081c3c37a74fddacaa07180acda430d
muratvuranok <muratvuranok@hotmail.com>
2019-10-26 12:14:14 +0300

    Add project files.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ctrl + R + G     (gereksiz usingleri siler)
+            // Ctrl + K + D     (kodları düzenler)
+            // Ctrl + K + C     (seçili kodları, yorum satırına alır)
+            // Ctrl + K + U     (seçili kodları, yorum satırından çıkartır)
+            // Ctrl + R + W     (wordwrap)
+            // Ctrl + Shift + B (build etme - derleme)
+
+
+            //Console.WriteLine("Hello World");
+            //Console.ReadKey(); // basılan tuşu teslim eder.
+            //Console.ReadLine(); // ekrana yazılan değeri teslim eder.
+
+
+            Console.WriteLine("Lütfen Adınızı  Giriniz : ");
+            Console.WriteLine("Giriş yapan kullanıcı : " + Console.ReadLine());
+
+            Console.Read();
+        }
+    }
+}
+
+
+
+// bu alana yazdığınız kodlar derlenecektir.
+
+/*  Çoklu yorum satırı */
